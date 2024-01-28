07289a3a19d5aef32729e8714dbded5b9a2462fd
dattha <dattha@tutamail.com>
2024-01-28 17:16:58 +0530

    Added examples for bash, python, go, rust programming lang in .bin dir
    Signed-off-by: dattha <dattha@tutamail.com>

---
+++ configurations/.bin/gofile.go
@@ -0,0 +1,10 @@
+// to compile the code "go build hello.go"
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World! from GO")
+}
+
---
+++ configurations/.bin/rustfile.rs
@@ -0,0 +1,6 @@
+//use "rustc rustfile.rs" to compile the file
+
+
+fn main() {
+    println!("Hello, World !! from rust");
+}
